a89684a798c0d1ed4ddc19903f62ed3c9d0050f8
JOEIH <joh73459@gmail.com>
2025-05-06 18:45:52 +0900

    💄 Design : 시작 페이지에 로고 추가 #2

--- frontend/src/pages/StartPage/index.tsx
+++ frontend/src/pages/StartPage/index.tsx
@@ -1,5 +1,11 @@
+import Logo from '../../assets/images/logo.png';
+
 const StartPage = () => {
-  return <div>시작 페이지</div>;
+  return (
+    <div>
+      <img src={Logo} alt='로고' width={440} />
+    </div>
+  );
 };
 
 export default StartPage;
