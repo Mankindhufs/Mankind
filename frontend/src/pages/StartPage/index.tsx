989d19167793fd707097b7e934ea05a8c150a53b
JOEIH <joh73459@gmail.com>
2025-05-07 03:11:46 +0900

    💄 Design : 파일 업로드 페이지 레이아웃 설정 #2

--- frontend/src/pages/StartPage/index.tsx
+++ frontend/src/pages/StartPage/index.tsx
@@ -4,8 +4,8 @@ import UploadBox from './components/UploadBox';
 const StartPage = () => {
   return (
     <div className='w-[100%] flex items-center justify-center h-[100%] px-10 box-border'>
-      <div className='w-[100%] h-[600px] flex gap-[110px] justify-center items-center'>
-        <div className='w-[400px] h-[220px] min-w-[200px] self-start'>
+      <div className='w-[100%] h-[700px] box-border flex gap-[10em] justify-center items-start'>
+        <div className='w-[400px] h-[220px] min-w-[200px]'>
           <img src={Logo} alt='로고' />
           Embracing a New Horizon in Finance!
         </div>
