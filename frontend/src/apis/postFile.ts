ce65fe9aa7f612a364baef35f9314986124552fa
JOEIH <joh73459@gmail.com>
2025-05-07 23:07:19 +0900

    ✨ Feat : 파일 업로드 api 생성 #3

---
+++ frontend/src/apis/postFile.ts
@@ -0,0 +1,11 @@
+import { axiosInstance } from './instance';
+
+// post pdf
+export const postFile = async (file: File) => {
+  const formData = new FormData();
+  formData.append('pdf', file);
+
+  const res = await axiosInstance.post('/api/scenario/upload', formData);
+
+  return res.data;
+};
