803407409d007f5922e588ba4246862ed29dfcde
JOEIH <joh73459@gmail.com>
2025-05-07 23:06:23 +0900

    ✨ Feat : axios instance 서버 주소 재설정

--- frontend/src/apis/instance.ts
+++ frontend/src/apis/instance.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 export const axiosInstance: AxiosInstance = axios.create({
-  baseURL: 'server',
+  baseURL: import.meta.env.VITE_SERVER_URL,
   timeout: 5000,
   headers: {
     accept: 'application/json',
