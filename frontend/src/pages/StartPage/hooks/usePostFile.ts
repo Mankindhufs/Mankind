5506f92d816bbc43f7116f261611340eb5b84758
JOEIH <joh73459@gmail.com>
2025-05-08 20:43:14 +0900

    ✨ Feat : pdf 업로드 시 error 처리 추가 #3

--- frontend/src/pages/StartPage/hooks/usePostFile.ts
+++ frontend/src/pages/StartPage/hooks/usePostFile.ts
@@ -1,15 +1,20 @@
+import { useNavigate } from 'react-router-dom';
 import { postFile } from './../../../apis/postFile';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 const usePostFile = () => {
   const queryClient = useQueryClient();
+  const navigate = useNavigate();
 
   const mutation = useMutation({
     mutationFn: (file: File) => postFile(file),
     onSuccess: (data) => {
       console.log(data);
       queryClient.setQueryData(['pdfResult'], data);
-      console.log('post 성공');
+      navigate('/verify');
+    },
+    onError: (err) => {
+      console.log('업로드 실패: ', err);
     },
   });
 
