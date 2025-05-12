7ef9dac19512e4ebee5a73325df06efec6d3a8e2
JOEIH <joh73459@gmail.com>
2025-05-12 15:11:28 +0900

    💄 Design : 헤더 위치 고정 #2

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -9,10 +9,10 @@ const Layout = () => {
     <div className='w-[100vw] h-[100vh] bg-grayBackground'>
       <div className='mx-auto max-w-[1440px] h-[100%] bg-white'>
         {location.pathname != '/' && (
-          <div className='w-[100%] h-[64px] flex items-center pr-4 pl-6 py-1 justify-between'>
+          <div className='fixed w-[100%] h-[64px] flex items-center pr-4 pl-6 py-1 justify-between bg-white'>
             <img src={Logo} alt='로고' className='h-14' />
             {location.pathname == '/dashboard' && (
-              <div className='self-end flex gap-2 items-center min-w-[800px] justify-end'>
+              <div className='self-end flex items-center min-w-[800px] justify-end bg-white'>
                 <PiWarningFill size={20} className='text-yellow-400' />
                 <p className='font-bold text-red my-1'>
                   본 대시보드는 파생상품 투자를 돕기 위한 어시스턴트 프로그램에
@@ -22,7 +22,9 @@ const Layout = () => {
             )}
           </div>
         )}
-        <Outlet />
+        <div className='pt-[64px]'>
+          <Outlet />
+        </div>
       </div>
     </div>
   );
