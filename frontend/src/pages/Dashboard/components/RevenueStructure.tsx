8c9eb510488ce5a6e2b2dfedf16403c161034a50
JOEIH <joh73459@gmail.com>
2025-05-13 04:13:21 +0900

    💄 Design : 공통 컴포넌트 이용해 대시보드 아이템 구성 #2

---
+++ frontend/src/pages/Dashboard/components/RevenueStructure.tsx
@@ -0,0 +1,11 @@
+import DashboardItem from './DashboardItem';
+
+const RevenueStructure = () => {
+  return (
+    <DashboardItem title='수익구조 분석 차트'>
+      <div>내용</div>
+    </DashboardItem>
+  );
+};
+
+export default RevenueStructure;
