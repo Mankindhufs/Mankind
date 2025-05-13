47bf80e5260e263c1fdfb26f2b62eff677723cd9
JOEIH <joh73459@gmail.com>
2025-05-13 23:40:34 +0900

    💄 Design : 대시보드 그래프 요소에 높이 설정 추가 #2

--- frontend/src/pages/Dashboard/components/DashboardItem.tsx
+++ frontend/src/pages/Dashboard/components/DashboardItem.tsx
@@ -9,13 +9,13 @@ const DashboardItem = ({
 }) => {
   return (
     <section className='w-full h-full rounded-[10px] shadow-md box-border'>
-      <div className='flex flex-col'>
+      <div className='flex flex-col w-full h-full'>
         <header className='flex gap-3 h-12 items-center p-3 border-b-2 border-grayBackground bg-white'>
           <hr className='h-6 w-1 bg-mainGreen' />
           <span className='font-semibold'>{title}</span>
         </header>
 
-        <main className='p-3'>{children}</main>
+        <main className='p-3 w-full h-[calc(100%-48px)]'>{children}</main>
       </div>
     </section>
   );
