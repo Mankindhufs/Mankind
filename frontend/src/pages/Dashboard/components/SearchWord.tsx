797ccfedd097cb48f87616ab449501b631ef3b0e
JOEIH <joh73459@gmail.com>
2025-06-03 04:07:51 +0900

    ✨ Feat : 검색어가 공백일 때 alert창 추가

--- frontend/src/pages/Dashboard/components/SearchWord.tsx
+++ frontend/src/pages/Dashboard/components/SearchWord.tsx
@@ -6,6 +6,7 @@ import {
   SetStateAction,
   useState,
 } from 'react';
+import Alert from '../../../components/Alert';
 const SearchWord = ({
   onSetWord,
   onSetModal,
@@ -14,6 +15,9 @@ const SearchWord = ({
   onSetModal: Dispatch<SetStateAction<boolean>>;
 }) => {
   const [inputWord, setInputWord] = useState<string>('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [showAlert, setShowAlert] = useState(false);
+  const [animation, setAnimation] = useState('');
 
   const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
     let searchedWord = e.target.value;
@@ -27,16 +31,34 @@ const SearchWord = ({
     console.log(validWord);
 
     if (!validWord) {
-      console.log('검색어를 다시 입력해주세요.');
+      setErrorMessage('공백만으로 이루어진 단어는 입력할 수 없습니다.');
+      setShowAlert(true);
+      setAnimation('animate-showAlert');
       return;
     }
 
     onSetWord(validWord);
     onSetModal(true);
+    handleHideAlert();
+  };
+
+  const handleHideAlert = () => {
+    if (showAlert && animation === 'animate-showAlert') {
+      setAnimation('animate-hideAlert');
+    }
   };
 
   return (
     <>
+      {showAlert && (
+        <Alert
+          animation={animation}
+          alertMessage='검색어가 존재하지 않습니다.'
+          errorMessage={errorMessage}
+          hideAlertFunction={handleHideAlert}
+          setAlert={setShowAlert}
+        />
+      )}
       <form
         onSubmit={handleSubmit}
         className='flex gap-1 bg-white p-3 w-full rounded-[10px] text-sm justify-between shadow-md'
